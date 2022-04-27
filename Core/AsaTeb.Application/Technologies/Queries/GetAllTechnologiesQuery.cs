64ee0a8dfe2c2a4ac4d7914dbd065f57eb03a972
hamidnch2007 <hamidnch2007@gmail.com>
2022-04-27 15:51:22 +0430

    GetallTechnologyQuery updated

--- Core/AsaTeb.Application/Technologies/Queries/GetAllTechnologiesQuery.cs
+++ Core/AsaTeb.Application/Technologies/Queries/GetAllTechnologiesQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading.Tasks;
-using AsaTeb.Application.Technologies.Dtos;
+﻿using AsaTeb.Application.Technologies.Dtos;
 using AsaTeb.Application.Technologies.Repositories;
 using AsaTeb.Domain.Technologies;
 using AutoMapper;
@@ -12,9 +6,9 @@ using MediatR;
 
 namespace AsaTeb.Application.Technologies.Queries
 {
-    public record GetAllTechnologiesQuery: IRequest<IList<TechnologyDto>>
+    public record GetAllTechnologiesQuery : IRequest<IList<TechnologyDto>>
     {
-        public record GetAllTechnologiesQueryHandler: IRequestHandler<GetAllTechnologiesQuery, IList<TechnologyDto>>
+        public record GetAllTechnologiesQueryHandler : IRequestHandler<GetAllTechnologiesQuery, IList<TechnologyDto>>
         {
             private readonly ITechnologyRepository _technologyRepository;
             private readonly IMapper _mapper;
@@ -28,11 +22,14 @@ namespace AsaTeb.Application.Technologies.Queries
             public async Task<IList<TechnologyDto>> Handle(GetAllTechnologiesQuery request, CancellationToken cancellationToken)
             {
                 var technologies = await _technologyRepository.LoadAllTechnologiesAsync();
-                
+
+                if (technologies == null) return new List<TechnologyDto>();
+
                 var technologiesDto =
                     technologies.Select(t => _mapper.Map<Technology, TechnologyDto>(t)).ToList();
 
                 return technologiesDto;
+
             }
         }
     }
