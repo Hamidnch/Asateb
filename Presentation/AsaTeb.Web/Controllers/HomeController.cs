e8e007f9a6390e825741b4eeea100d2318a269a6
hamidnch2007 <hamidnch2007@gmail.com>
2022-04-27 15:55:10 +0430

    Add some action in Home controller for viewing Technologies and Candidates

--- Presentation/AsaTeb.Web/Controllers/HomeController.cs
+++ Presentation/AsaTeb.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using AsaTeb.Application.FacadePattern;
+﻿using AsaTeb.Application.Candidates.Dtos;
+using AsaTeb.Application.FacadePattern;
+using AsaTeb.Application.Technologies.Dtos;
+using AsaTeb.WebFramework.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AsaTeb.Web.Controllers
@@ -6,16 +10,31 @@ namespace AsaTeb.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IAsaTebService _asaTebService;
+        private readonly IMapper _mapper;
 
-        public HomeController(IAsaTebService asaTebService)
+        public HomeController(IAsaTebService asaTebService, IMapper mapper)
         {
             _asaTebService = asaTebService;
+            _mapper = mapper;
         }
 
-        public async Task<IActionResult> Index()
+        public Task<IActionResult> Index()
+        {
+            return Task.FromResult<IActionResult>(View());
+        }
+        public async Task<IActionResult> GetAllTechnologies()
         {
             var technologies = await _asaTebService.GetAllTechnologies();
-            return View(technologies);
+            var model =
+                technologies.Select(t => _mapper.Map<TechnologyDto, TechnologyModel>(t));
+            return View(model);
+        }
+
+        public async Task<IActionResult> GetAllCandidates()
+        {
+            var candidates = await _asaTebService.GetAllCandidates();
+            var model = candidates.Select(c => _mapper.Map<CandidateDto, CandidateModel>(c));
+            return View(model);
         }
     }
 }
