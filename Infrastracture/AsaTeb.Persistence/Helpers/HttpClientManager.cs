51b4021868bad1a774581943598e94ef531d5e11
hamidnch2007 <hamidnch2007@gmail.com>
2022-04-27 15:53:15 +0430

    Refactor HttpClientManager

--- Infrastracture/AsaTeb.Persistence/Helpers/HttpClientManager.cs
+++ Infrastracture/AsaTeb.Persistence/Helpers/HttpClientManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using AsaTeb.Application.Technologies.Dtos;
 using AsaTeb.Domain.Technologies;
 using Newtonsoft.Json;
 
@@ -13,9 +14,8 @@ namespace AsaTeb.Persistence.Helpers
     {
         private const string BaseUrl = "https://app.ifs.aero/EternalBlue/";
 
-        public static async Task<IEnumerable<Tech>?> ResolveUrl(string url)
+        public static async Task<IEnumerable<T>?> ResolveUrlAsync<T>(string url)
         {
-            //IEnumerable<T>? entities = new List<T>();
             using var client = new HttpClient();
             client.BaseAddress = new Uri(BaseUrl);
             client.DefaultRequestHeaders.Clear();
@@ -24,19 +24,33 @@ namespace AsaTeb.Persistence.Helpers
 
             res.EnsureSuccessStatusCode();
 
-            if (!res.IsSuccessStatusCode) return new List<Tech>();
+            if (!res.IsSuccessStatusCode) return new List<T>();
 
-            var techResponse = await res.Content.ReadAsStringAsync();
-            var entities = JsonConvert.DeserializeObject<IEnumerable<Tech>>(techResponse);
+            var response = await res.Content.ReadAsStringAsync();
+            var entities = JsonConvert.DeserializeObject<IEnumerable<T>>(response);
 
             return entities;
 
         }
-    }
 
-    public class Tech
-    {
-        public Guid Guid { get; set; }
-        public string? Name { get; set; }
+        //public static async Task<IEnumerable<TechnologyDto>?> ResolveUrl(string url)
+        //{
+        //    //IEnumerable<T>? entities = new List<T>();
+        //    using var client = new HttpClient();
+        //    client.BaseAddress = new Uri(BaseUrl);
+        //    client.DefaultRequestHeaders.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //    var res = await client.GetAsync(url);
+
+        //    res.EnsureSuccessStatusCode();
+
+        //    if (!res.IsSuccessStatusCode) return new List<TechnologyDto>();
+
+        //    var techResponse = await res.Content.ReadAsStringAsync();
+        //    var entities = JsonConvert.DeserializeObject<IEnumerable<TechnologyDto>>(techResponse);
+
+        //    return entities;
+
+        //}
     }
 }
