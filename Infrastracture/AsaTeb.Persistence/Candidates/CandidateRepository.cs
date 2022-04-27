c7dd8899afbded4b0d4c30cc91a3cf85c2c41a62
hamidnch2007 <hamidnch2007@gmail.com>
2022-04-27 15:51:04 +0430

    Implement ICandidateRepository in persistent layer

---
+++ Infrastracture/AsaTeb.Persistence/Candidates/CandidateRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsaTeb.Application.Candidates.Dtos;
+using AsaTeb.Application.Candidates.Repositories;
+using AsaTeb.Application.Technologies.Dtos;
+using AsaTeb.Domain.Candidates;
+using AsaTeb.Domain.Technologies;
+using AsaTeb.Persistence.Helpers;
+using AutoMapper;
+
+namespace AsaTeb.Persistence.Candidates
+{
+    public class CandidateRepository: ICandidateRepository
+    {
+        private readonly IMapper _mapper;
+
+        public CandidateRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Candidate>?> LoadAllCandidatesAsync()
+        {
+            var candidates = await HttpClientManager.ResolveUrlAsync<CandidateDto>("api/candidates");
+            var res = candidates?.Select(t => _mapper.Map<CandidateDto, Candidate>(t));
+            return res;
+        }
+
+        public async Task<IEnumerable<Candidate>> GetCandidatesByTechnologyIdAsync(Guid technologyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Experience>> GetExperiencesByCandidateIdAsync(Guid candidateId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
