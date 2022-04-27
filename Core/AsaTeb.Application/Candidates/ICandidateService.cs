bf1f32ac9d51f383cf18034dd0b4aba1cc650d28
hamidnch2007 <hamidnch2007@gmail.com>
2022-04-27 15:49:28 +0430

    Rename ICandidateService to ICandicateRepository and move to Repositories folder

--- Core/AsaTeb.Application/Candidates/ICandidateService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AsaTeb.Application.Candidates;
-
-public interface ICandidateService
-{
-    Task LoadAllCandidates();
-    Task<IList<CandidateDto>> GetCandidatesByTechnologyId(Guid technologyId);
-    Task<IList<ExperienceDto>> GetExperiencesByCandidateId(Guid candidateId);
-}
---
+++ Core/AsaTeb.Application/Candidates/Repositories/ICandidateRepository.cs
@@ -0,0 +1,10 @@
+﻿using AsaTeb.Domain.Candidates;
+
+namespace AsaTeb.Application.Candidates.Repositories;
+
+public interface ICandidateRepository
+{
+    Task<IEnumerable<Candidate>?> LoadAllCandidatesAsync();
+    Task<IEnumerable<Candidate>> GetCandidatesByTechnologyIdAsync(Guid technologyId);
+    Task<IEnumerable<Experience>> GetExperiencesByCandidateIdAsync(Guid candidateId);
+}
